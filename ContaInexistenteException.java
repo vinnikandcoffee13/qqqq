fd972265da4ea5cc768e8d26ca6fbf8ce2e6d3b6
Only Viny, the code <62970703+vinnikandcoffee13@users.noreply.github.com>
2023-10-20 18:15:58 -0300

    Add files via upload

---
+++ ContaInexistenteException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author 20212SI0027
+ */
+public class ContaInexistenteException extends Exception{
+    public ContaInexistenteException(String st){
+        super(st);
+    }
+}
